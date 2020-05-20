2232621435f563992efd5eed9a6eed8c8db94920
Alexander Kriegisch <Alexander@Kriegisch.name>
2020-05-20 14:44:46 +0700

    Indentation cosmetics

--- remove-final-parent/remove-final-agent/src/main/java/de/scrum_master/agent/remove_final/RemoveFinalTransformer.java
+++ remove-final-parent/remove-final-agent/src/main/java/de/scrum_master/agent/remove_final/RemoveFinalTransformer.java
@@ -87,32 +87,32 @@ public class RemoveFinalTransformer extends ClassVisitor {
     return
       // Our own agent-related stuff
       !className.startsWith("de.scrum_master.agent.")
-      // The JVM does not tolerate definalisation ob class Object but says:
-      //   Error occurred during initialization of VM
-      //   Incompatible definition of java.lang.Object
-      && !className.equals("java.lang.Object")
-      // Byte code engineering
-      && !className.startsWith("net.bytebuddy.")
-      && !className.startsWith("org.objectweb.asm.")
-      && !className.startsWith("groovyjarjarasm.asm.")
-      && !className.startsWith("javassist.")
-      && !className.startsWith("org.objenesis.")
-      && !className.contains("$$EnhancerByCGLIB$$")
-      // Testing
-      && !className.startsWith("org.junit.")
-      && !className.startsWith("junit.")
-      && !className.startsWith("org.hamcrest.")
-      && !className.startsWith("org.spockframework.")
-      && !className.startsWith("spock.")
-      // Mocking
-      && !className.startsWith("org.mockito.")
-      && !className.startsWith("mockit.")
-      && !className.startsWith("org.powermock.")
-      && !className.startsWith("org.easymock.")
-      // Build
-      && !className.startsWith("org.apache.maven.")
-      // IDE
-      && !className.startsWith("com.intellij.")
+        // The JVM does not tolerate definalisation ob class Object but says:
+        //   Error occurred during initialization of VM
+        //   Incompatible definition of java.lang.Object
+        && !className.equals("java.lang.Object")
+        // Byte code engineering
+        && !className.startsWith("net.bytebuddy.")
+        && !className.startsWith("org.objectweb.asm.")
+        && !className.startsWith("groovyjarjarasm.asm.")
+        && !className.startsWith("javassist.")
+        && !className.startsWith("org.objenesis.")
+        && !className.contains("$$EnhancerByCGLIB$$")
+        // Testing
+        && !className.startsWith("org.junit.")
+        && !className.startsWith("junit.")
+        && !className.startsWith("org.hamcrest.")
+        && !className.startsWith("org.spockframework.")
+        && !className.startsWith("spock.")
+        // Mocking
+        && !className.startsWith("org.mockito.")
+        && !className.startsWith("mockit.")
+        && !className.startsWith("org.powermock.")
+        && !className.startsWith("org.easymock.")
+        // Build
+        && !className.startsWith("org.apache.maven.")
+        // IDE
+        && !className.startsWith("com.intellij.")
       ;
   }
 
