6a9cdeefca3b2e416c4fbbbd4b38a7d9e082cab7
Alexander Kriegisch <Alexander@Kriegisch.name>
2020-05-23 09:14:36 +0700

    Rename enum TargetType to AdviceScope and document its constants

--- aspect-parent/aspect-core/src/main/java/de/scrum_master/agent/aspect/Aspect.java
+++ aspect-parent/aspect-core/src/main/java/de/scrum_master/agent/aspect/Aspect.java
@@ -7,7 +7,7 @@ import net.bytebuddy.matcher.ElementMatcher;
 
 import java.util.*;
 
-import static de.scrum_master.agent.aspect.Aspect.TargetType.*;
+import static de.scrum_master.agent.aspect.Aspect.AdviceScope.*;
 import static net.bytebuddy.matcher.ElementMatchers.*;
 
 public abstract class Aspect<T> {
@@ -15,15 +15,64 @@ public abstract class Aspect<T> {
 
   private static final ClassFileLocator CLASS_FILE_LOCATOR = ClassFileLocator.ForClassLoader.ofSystemLoader();
 
-  public enum TargetType {
-    GLOBAL, MODULE, PACKAGE, CLASS, METHOD, INSTANCE
+  /**
+   * A concrete advice can be registered with different scopes, e.g. per class or for a specific object instance.
+   * See the description of each defined enum constant for more details.
+   */
+  public enum AdviceScope {
+    /**
+     * Apply advice globally. I.e whatever the class and method matchers of the defined aspect match, however broadly
+     * they are defined, the advice will be applied, unless a less global scope is registered on the weaver.
+     * <p>
+     * <i>Not implemented yet.</i>
+     */
+    GLOBAL,
+    /**
+     * Limit advice application to anything matched within a specific Java module.
+     * <p>
+     * <i>Not implemented yet.</i>
+     */
+    MODULE,
+    /**
+     * Limit advice application to anything matched within a specific package and its subpackages.
+     * <p>
+     * <i>Not implemented yet.</i>
+     * <p>
+     * TODO: Modules and packages can intersect. How do we handle this?
+     */
+    PACKAGE,
+    /**
+     * Limit advice application to anything matched within a specific class.
+     * <p>
+     * <i>Not implemented yet.</i>
+     * <p>
+     * TODO: What about subclasses? Probably we shall not support them. Why recreate Aspect? This framework is mostly
+     *       targeted to people looking for a mocking tool which can also be applied to already loaded classes, even
+     *       JRE bootstrap classes.
+     */
+    CLASS,
+    /**
+     * Limit advice application to anything matching a specific method or constructor with a unique signature, i.e.
+     * explicitly do not match multiple overloaded methods of the same name.
+     * <p>
+     * <i>Not implemented yet.</i>
+     */
+    METHOD,
+    /**
+     * Limit advice application to a specific instance registered on the weaver.
+     * <p>
+     * <i>Not implemented yet.</i>
+     * <p>
+     * TODO: Instance scope intersecs with all other scopes. How do we handle preference?
+     */
+    INSTANCE
   }
 
   public enum AdviceType {
     METHOD_ADVICE(
       Advice.to(MethodAspect.class, CLASS_FILE_LOCATOR),
       isMethod(),
-      GLOBAL, MODULE, PACKAGE, CLASS, INSTANCE
+      GLOBAL, MODULE, PACKAGE, CLASS, METHOD, INSTANCE
     ),
     CONSTRUCTOR_ADVICE(
       Advice.to(ConstructorAspect.class, CLASS_FILE_LOCATOR),
@@ -39,9 +88,9 @@ public abstract class Aspect<T> {
 
     private final Advice advice;
     private final ElementMatcher.Junction<MethodDescription> methodType;
-    private final SortedSet<TargetType> allowedTargetTypes = new TreeSet<>();
+    private final SortedSet<AdviceScope> allowedTargetTypes = new TreeSet<>();
 
-    AdviceType(Advice advice, ElementMatcher.Junction<MethodDescription> methodType, TargetType... allowedTargetTypes) {
+    AdviceType(Advice advice, ElementMatcher.Junction<MethodDescription> methodType, AdviceScope... allowedTargetTypes) {
       this.advice = advice;
       this.methodType = methodType;
       Collections.addAll(this.allowedTargetTypes, allowedTargetTypes);
@@ -55,7 +104,7 @@ public abstract class Aspect<T> {
       return methodType;
     }
 
-    public SortedSet<TargetType> getAllowedTargetTypes() {
+    public SortedSet<AdviceScope> getAllowedTargetTypes() {
       return allowedTargetTypes;
     }
   }
