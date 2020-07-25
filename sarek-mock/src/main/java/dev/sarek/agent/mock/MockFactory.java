7de62e41bfbe650bf756ad29fcf77ba0f3f81928
Alexander Kriegisch <Alexander@Kriegisch.name>
2020-07-25 16:32:55 +0700

    Start writing JavaDoc for MockFactory and MockType (work in progress)

--- sarek-mock/src/main/java/dev/sarek/agent/mock/MockFactory.java
+++ sarek-mock/src/main/java/dev/sarek/agent/mock/MockFactory.java
@@ -14,6 +14,76 @@ import java.util.Set;
 
 import static net.bytebuddy.matcher.ElementMatchers.*;
 
+/**
+ * A Sarek mock factory and its corresponding builder class are a high level wrapper around other, lower-level Sarek
+ * functionalities such as constructor mocking and aspect advices. Sarek mocks/spies are not meant to replace other mock
+ * frameworks such as Mockito, EasyMock or the mock functionality contained in the Spock framework, but to complement
+ * them. While there are some functional intersections, some key features go beyond normal mock frameworks. They might
+ * not be as nice and easy to use as the mentioned frameworks, but they do work and hopefully offer added value for
+ * difficult test cases. Some basic functionalities are:
+ * <ol>
+ *   <li>
+ *     Mock final classes: Sarek does not use dynamic proxies and thus does not rely on subclassing. Instead, it
+ *     directly instruments target classes. Thus, the {@code final} qualifier on classes is not a show stopper.
+ *   </li>
+ *   <li>
+ *     Stub final methods: For the same reasons as above, Sarek can directly instrument final methods and thus stub them
+ *     or spy on them.
+ *   </li>
+ *   <li>
+ *     Mock, stub and spy on already loaded classes, including most JRE classes: Because Sarek instrumentation does not
+ *     change class structure, in-memory instumentation on already loaded classes (<i>retransformation</i> in technical
+ *     terms), including JRE bootstrap classes, can be performed.
+ *   </li>
+ *   <li>
+ *     Mock, stub and spy functionality can be limited to single instances or applied globally (similar to Spock's
+ *     global Groovy mocks, but applicable to Java, Kotlin or other JVM language classes too). I.e., it is possible to
+ *     intercept constructor calls on registered target classes and dynamically skip the constructor code, effectively
+ *     creating an uninitialised instance. In combination with method mocking/stubbing, this enables users to mock
+ *     non-injectable objects, for example objects created outside the control of a test inside a method, directly
+ *     instantiated with {@code new}.
+ *   </li>
+ *   <li>
+ *     For said global, non-injectable mocks it is possible to also obtain references to them by polling a queue of
+ *     created mock objects per target class. This way, even though the target object has been created outside the
+ *     control of a test, the user can still grab a reference and then interact with it. In many cases this is not
+ *     necessary, but when it is, this feature comes in handy.
+ *   </li>
+ *   <li>
+ *     Method stubbing is not limited to instance methods, but also encompasses static methods.
+ *   </li>
+ *   <li>
+ *     All of Sarek's mock, stub, spy byte code transformations are reversible, i.e. all target classes can be reset to
+ *     their original versions by just calling {@code #close} on the mock factory or using try-with-resources because
+ *     this class implements the {@code AutoCloseable} interface.
+ *   </li>
+ *   <li>
+ *     It is also possible to manually create mock instances via {@code #createInstance}. In this case, Sarek does not
+ *     use constructor instrumentation but simply uses <i>Objenesis</i> in order to create an instance. This is similar
+ *     to what other mock frameworks do in certain cases, but in combination with method stubbing it also works for
+ *     final classes, as mentioned above.
+ *   </li>
+ * </ol>
+ * <p></p>
+ * The generic usage pattern is:
+ * <ol>
+ *   <li>Create a {@link Builder} via {@link #forClass(Class)} or {@link #forClass(String)}</li>
+ *   <li>Configure the builder, calling a chained set of builder methods</li>
+ *   <li>Create a {@link MockFactory} instance via {@link Builder#build()}</li>
+ *   <li>
+ *     Create and interact with mock instances, either directly by calling methods on the mock factory or on the created
+ *     mocks as such.
+ *   </li>
+ *   <li>
+ *     Close the mock factory, either via try-with-resources or by directly calling {@link #close()}. This reverts the
+ *     corresponding code instrumentation for the target class and also unregisters all existing mocks, empties global
+ *     mock polling queues and frees other resources.
+ *   </li>
+ * </ol>
+ * For more information, please look at Sarek's own test suite or read the tutorial.
+ *
+ * @param <T> target type to create mock/spy opjects for
+ */
 public class MockFactory<T> implements AutoCloseable {
   private Class<T> targetClass;
   private Weaver weaver;
@@ -141,7 +211,23 @@ public class MockFactory<T> implements AutoCloseable {
 
   }
 
-  public enum MockType {MOCK, SPY}
+  /**
+   * There are two basic types of test doubles: mock and spy. See enum value documentation for more details.
+   */
+  public enum MockType {
+    /**
+     * A Sarek mock is an uninitialised target class instance (not a subclass or a proxy), instrumented to be a mock
+     * object. Its default behaviour for called methods is to return a dummy "null-ish" value like {@code null},
+     * {@code 0} or {@code false}. This behaviour can be modified on a per-method basis.
+     */
+    MOCK,
+    /**
+     * A Sarek spy is based on a fully initialised target class instance (not a subclass or a proxy), instrumented to be
+     * a spy object. Its default behaviour for called methods is to pass through each call to the original method and
+     * return the original result. This behaviour can be modified on a per-method basis.
+     */
+    SPY
+  }
 
   private MockFactory(Builder<T> builder) {
     targetClass = builder.targetClass;
